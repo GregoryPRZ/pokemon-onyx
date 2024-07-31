ecef8b2f194fcdfb01f70898867e787298fede1c
GregoryPRZ <gregoryp161@gmail.com>
2024-07-31 10:13:07 +0200

    Creator npc trainer test (will be done later)

--- include/constants/trainers.h
+++ include/constants/trainers.h
@@ -351,7 +351,9 @@
 #define TRAINER_CLASS_PIKE_QUEEN     0x3f
 #define TRAINER_CLASS_PYRAMID_KING   0x40
 #define TRAINER_CLASS_RS_PROTAG      0x41
-#define TRAINER_CLASS_COUNT          0x42
+#define TRAINER_CLASS_CREATOR 0x42  // Unused
+#define TRAINER_CLASS_LEGEND 0x43 // Unused
+#define TRAINER_CLASS_COUNT          0x44
 
 #define TRAINER_ENCOUNTER_MUSIC_MALE         0 // standard male encounter music
 #define TRAINER_ENCOUNTER_MUSIC_FEMALE       1 // standard female encounter music
