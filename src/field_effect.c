179d26028ed99fd808073eec435c8bdc9ad460eb
BlayzeRod <gregoperez995@gmail.com>
2024-08-20 21:39:33 +0200

    fix fly

--- src/field_effect.c
+++ src/field_effect.c
@@ -3247,7 +3247,7 @@ static void FlyingTaxiFieldEffect_FlyNoises(struct Task *task)
 static void Task_FlyOut(u8 taskId)
 {
     sFlyOutFieldEffectFuncs[gTasks[taskId].tState](&gTasks[taskId]);
-    if (VarGet(VAR_0x800A) == LAST_TALKED_TO_FLYING_TAXI)
+    if (VarGet(VAR_LAST_TALKED_TO_FLYING_TAXI) == LAST_TALKED_TO_FLYING_TAXI)
         sFlyingTaxiFieldEffectFuncs[gTasks[taskId].tState](&gTasks[taskId]);
     else
         sFlyOutFieldEffectFuncs[gTasks[taskId].tState](&gTasks[taskId]);
@@ -3360,7 +3360,6 @@ static void FlyOutFieldEffect_End(struct Task *task)
 {
     if (!gPaletteFade.active)
     {
-        VarSet(VAR_0x800A, 0);
         FieldEffectActiveListRemove(FLDEFF_USE_FLY);
         DestroyTask(FindTaskIdByFunc(Task_FlyOut));
     }
